2501cddaa9fce15d3ecdea55a41cbd459925ec71
Silviu Bogan <silviubogan@gmail.com>
2020-08-27 16:04:24 +0300

    Added doc comments

--- src/Widget/ObjectWidget.jsx
+++ src/Widget/ObjectWidget.jsx
@@ -4,6 +4,19 @@ import { Tab } from 'semantic-ui-react';
 
 import Field from '@plone/volto/components/manage/Form/Field';
 
+/**
+ * Renders a field set. Passes some of the values in the schema to the Field
+ * component used inside. Shows the current value, the errors, the required
+ * status of the fields inside.
+ *
+ * @param {object} data
+ * @param {number} index
+ * @param {object} schema
+ * @param {object} value
+ * @param {object} errors
+ * @param {function} onChange
+ * @param {string} id
+ */
 const FieldSet = ({ data, index, schema, value, errors, onChange, id }) => {
   return data.fields.map((field, idx) => {
     return (
@@ -24,6 +37,18 @@ const FieldSet = ({ data, index, schema, value, errors, onChange, id }) => {
   });
 };
 
+/**
+ * Returns a tab pane with a field set in it. All the props are passed on to the
+ * FieldSet component defined above.
+ *
+ * @param {object} fieldSetData
+ * @param {number} index
+ * @param {object} schema
+ * @param {object} errors
+ * @param {object} value
+ * @param {function} onChange
+ * @param {string} id
+ */
 const MyTabPane = ({
   fieldSetData,
   index,
@@ -48,6 +73,17 @@ const MyTabPane = ({
   );
 };
 
+/**
+ * Creates an object widget with the given onChange handler and an ID. If there
+ * are multiple field sets, it renders a Tab component with multiple tab panes.
+ * Each tab has the title of the fieldset it renders.
+ *
+ * @param {object} schema
+ * @param {object} value
+ * @param {function} onChange
+ * @param {object} errors
+ * @param {string} id
+ */
 const ObjectWidget = ({
   schema,
   value, // not checked to not contain unknown fields
