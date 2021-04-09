6bde47affa864cb1d01601f4399fc1ce168e0c65
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-04-09 15:16:49 +0300

    Rename object_list -> object_list_popup, as Volto now comes with ObjectList widget

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ export installDemo from './demo';
 
 const applyConfig = (config) => {
   config.widgets.widget.object = ObjectWidget;
-  config.widgets.widget.object_list = ObjectListWidget;
+  config.widgets.widget.object_list_popup = ObjectListWidget;
   config.widgets.widget.object_list_inline = ObjectListInlineWidget;
   config.widgets.widget.object_by_type = ObjectByTypeWidget;
   config.widgets.widget.option_mapping = MappingWidget;
