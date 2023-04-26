b620c98af86f13345d6d895a0755c6cebb981986
andreiggr <andrei.ggr@gmail.com>
2023-04-26 16:49:13 +0300

    move object types widget and adapt to volto-resize-helper

--- src/Widget/index.js
+++ src/Widget/index.js
@@ -3,3 +3,4 @@ export ObjectListWidget from './ObjectListWidget';
 export ObjectListInlineWidget from './ObjectListInlineWidget';
 export ObjectByTypeWidget from './ObjectByTypeWidget';
 export MappingWidget from './MappingWidget';
+export ObjectTypesWidget from './ObjectTypesWidget';
