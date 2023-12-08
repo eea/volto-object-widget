181438475532646d4bb07d651a2e09f31cf085bb
Miu Razvan <miu.razvan28@gmail.com>
2023-12-08 17:49:19 +0200

    feat: enhance UrlWidget/InternalUrlWidget

--- src/Widget/index.js
+++ src/Widget/index.js
@@ -5,3 +5,5 @@ export { default as ObjectByTypeWidget } from './ObjectByTypeWidget';
 export { default as MappingWidget } from './MappingWidget';
 export { default as ObjectTypesWidget } from './ObjectTypesWidget';
 export { default as AttachedFileWidget } from './AttachedFileWidget';
+export { default as UrlWidget } from './UrlWidget';
+export { default as InternalUrlWidget } from './InternalUrlWidget';
