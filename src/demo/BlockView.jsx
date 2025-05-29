0a2b61f6beeb81ecc7fb8b8b211b162842b0976d
David Ichim <ichim.david@gmail.com>
2025-05-29 22:58:53 +0300

    WIp adding cypress tests for attachedimage
    - trying to test with unittests makes it very hard to test interactions such as drag and drop or modals

--- src/demo/BlockView.jsx
+++ src/demo/BlockView.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import { getImageScaleParams } from '@eeacms/volto-object-widget/helpers';
+import config from '@plone/volto/registry';
 
-const BlockView = (props) => <div>{JSON.stringify(props.data)}</div>;
+const ItemImage = ({ image, imageSize, verticalAlign }) => {
+  if (!image) return null;
+  const scaledImage = getImageScaleParams(image, imageSize);
+  const imageSizes = config.blocks?.blocksConfig?.item?.imageSizes ?? {};
+  const size = imageSizes[imageSize] ?? { width: undefined, height: undefined };
+
+  return (
+    <img
+      src={scaledImage?.download}
+      className={`ui image ${imageSize} ${verticalAlign} aligned`}
+      alt=""
+      width={size.width}
+      height={size.height}
+      loading="lazy"
+    />
+  );
+};
+
+const BlockView = (props) => (
+  <div>
+    <p>{JSON.stringify(props.data)} </p>
+    <ItemImage
+      image={props.data.image}
+      imageSize={props.data.imageSize || 'large'}
+      verticalAlign={props.data.verticalAlign || 'middle'}
+    />
+  </div>
+);
 
 export default BlockView;
